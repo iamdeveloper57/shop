e939e61d319ed45d7eddeac5326ac7c74fff9c45
developer <agraharirehan57@gmail.com>
2024-07-11 16:30:33 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// document.querySelector(".card").addEventListener('click', ()=>{
+//     document.querySelector(".card-body").style.display = "block";
+//     document.querySelector(".post").style.display = "none";
+// })
