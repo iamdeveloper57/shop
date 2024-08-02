8a0615e8a78e67339c87e0e08bbac166ed65104b
developer <agraharirehan57@gmail.com>
2024-08-02 11:14:38 +0545

    add search featers

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-// document.querySelector(".card").addEventListener('click', ()=>{
-//     document.querySelector(".card-body").style.display = "block";
-//     document.querySelector(".post").style.display = "none";
-// })
+const onSearch = () =>{
+    const input = document.querySelector(".input");
+    const filter = input.value.toUpperCase();
+    
+    const list = document.querySelectorAll(".card");
+    list.forEach((el) => {
+        const text = el.textContent.toUpperCase();
+        
+      if(text.includes(filter)){
+        el.style.display = ""
+      }
+      else{
+        el.style.display = "none"
+        
+      }
+    
+    });
+    
+    };
+    
+    document.querySelector("input").addEventListener("input", onSearch);
